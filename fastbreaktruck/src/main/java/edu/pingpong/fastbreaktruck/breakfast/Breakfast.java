4606388c517be69ca5c5f04e4df2ebfc867ccc94
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-04-30 17:23:30 +0200

    [IMP]: Formateo de código

--- fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/breakfast/Breakfast.java
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/breakfast/Breakfast.java
@@ -7,24 +7,25 @@ import edu.pingpong.fastbreaktruck.interfaces.Product;
 
 public class Breakfast {
     private List<Product> products = new ArrayList<Product>();
-    public void addProduct(Product product){
+
+    public void addProduct(Product product) {
         products.add(product);
     }
 
-    public float getCost(){
+    public float getCost() {
         float cost = 0.0f;
-        for (Product product : products){
+        for (Product product : products) {
             cost += product.price();
         }
         return cost;
     }
 
-    public void showProduct(){
-        for (Product product : products){
+    public void showProduct() {
+        for (Product product : products) {
             System.out.println("Item : " + product.name());
             System.out.println(", Packing : " + product.packing().pack());
             System.out.println(", Price : " + product.price());
         }
     }
-    
+
 }
