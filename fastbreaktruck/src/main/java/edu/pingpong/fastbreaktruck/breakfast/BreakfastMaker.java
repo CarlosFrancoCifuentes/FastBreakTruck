a6636dd13717c4d2b292146416a7bd34780a2286
CarlosFrancoCifuentes <72622430+CarlosFrancoCifuentes@users.noreply.github.com>
2021-04-30 17:24:52 +0200

    [ADD]: Fábrica de desayunos

---
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/breakfast/BreakfastMaker.java
@@ -0,0 +1,23 @@
+package edu.pingpong.fastbreaktruck.breakfast;
+
+import edu.pingpong.fastbreaktruck.cereals.Eyeholes;
+import edu.pingpong.fastbreaktruck.cereals.Smiggles;
+import edu.pingpong.fastbreaktruck.drink.Fleebjuice;
+import edu.pingpong.fastbreaktruck.drink.Turbulentjuice;
+
+public class BreakfastMaker {
+
+    public static Breakfast prepareEyeholes() {
+        Breakfast breakfast = new Breakfast();
+        breakfast.addProduct(new Eyeholes());
+        breakfast.addProduct(new Turbulentjuice());
+        return breakfast;
+    }
+
+    public static Breakfast prepareSmiggles() {
+        Breakfast breakfast = new Breakfast();
+        breakfast.addProduct(new Smiggles());
+        breakfast.addProduct(new Fleebjuice());
+        return breakfast;
+    }
+}
